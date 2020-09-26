b192c9fc8812d08d27bae6cf7dee85b2455b72f7
Piero Divasto <pdivasto@gmail.com>
2020-09-26 14:58:37 -0300

    Dockerize the app

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const APP_PORT = process.env.APP_PORT || 3000;
+
 const server = require("./app")({
   logger: {
     level: "info",
@@ -7,9 +9,11 @@ const server = require("./app")({
   },
 });
 
-server.listen(3000, (err) => {
+server.listen(APP_PORT, "0.0.0.0", (err, address) => {
   if (err) {
     console.log(err);
     process.exit(1);
   }
+
+  server.log.info(`server listening on ${address}`);
 });
