faa9345725e83bea92cd95c58f6a7d56dd4efc27
Piero Divasto <pdivasto@gmail.com>
2020-09-26 16:58:19 -0300

    Install mongoose as plugin

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 "use strict";
 
 const APP_PORT = process.env.APP_PORT || 3000;
+const DB_USERNAME = process.env.DB_USERNAME || "admin";
+const DB_PASSWORD = process.env.DB_PASSWORD || "secret";
+const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_PORT = process.env.DB_PORT || "27017";
 
 const server = require("./app")({
   logger: {
@@ -9,11 +13,27 @@ const server = require("./app")({
   },
 });
 
-server.listen(APP_PORT, "0.0.0.0", (err, address) => {
-  if (err) {
-    console.log(err);
-    process.exit(1);
+server.register(
+  require("fastify-mongoose-driver").plugin,
+  {
+    uri: `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`,
+    settings: {
+      useNewUrlParser: true,
+      config: {
+        autoIndex: true,
+      },
+    },
+    useNameAndAlias: true,
+  },
+  (err) => {
+    if (err) throw err;
   }
+);
 
-  server.log.info(`server listening on ${address}`);
-});
+server
+  .ready()
+  .then(() => server.listen(APP_PORT, "0.0.0.0"))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
